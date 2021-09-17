1e787f66af7d3bd2b922a8f25e95ad495dfc2cf5
Sakhnik Arseni <sakhnikarseni@mail.ru>
2021-09-18 00:39:03 +0300

    добавил vue в разор пагес

--- src/NorthWind.API/Startup.cs
+++ src/NorthWind.API/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace NorthWind.API
@@ -23,6 +25,7 @@ namespace NorthWind.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
             services.AddRazorPages();
         }
 
@@ -36,20 +39,26 @@ namespace NorthWind.API
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "clientapp")),
+                RequestPath = "/clientapp"
+            });
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
---
+++ src/NorthWind.API/clientapp/vue.config.js
@@ -0,0 +1,8 @@
+﻿const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+    outputDir: path.resolve('../wwwroot/bundles/'),
+    filenameHashing: false,
+    runtimeCompiler: true,
+}
