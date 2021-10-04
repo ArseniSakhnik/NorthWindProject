281932bd6c8cbad7a74a426d01ee2e843947bcd6
Sakhnik Arseni <sakhnikarseni@mail.ru>
2021-10-04 18:35:02 +0300

    Господи ну почему ничего не работает

--- src/NorthWind.API/clientapp/src/main.ts
+++ src/NorthWind.API/clientapp/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router"
 import '@/css/index.css'
+import '@/css/services1.jpg'
+import '@/fonts/Raleway-Light.ttf'
 
 createApp(App)
     .use(router)    
