d0457cb958a8a0f7a546107fb0f11028293af66e
sakhn <sakhnikarseni@mail.ru>
2022-01-20 06:21:11 +0300

    # Добавил httpService

--- src/NorthWind.API/clientapp/src/services/HttpService.ts
+++ src/NorthWind.API/clientapp/src/services/HttpService.ts
@@ -8,24 +8,66 @@ export default abstract class HttpService {
 		this.basePath += basePath
 	}
 
-	protected baseRequest<TBody, TResponse>(path: string, methodType: MethodType, contentType: ContentType, body: TBody | null = null): Promise<TResponse> {
-		const requestInit: RequestInit = {
-			credentials: 'include',
-			method: methodType,
-			headers: {
-				'Content-type': contentType
-			},
-		}
-
-		if (methodType === 'POST' || methodType === 'PUT') {
-			requestInit.body = JSON.stringify(body);
-		}
+	private baseRequest = async <TResponse extends unknown>
+	(path: string, methodType: MethodType, body: any = null): Promise<TResponse> => {
 		
-		const request = fetch(path, requestInit)
-			.then(response => response.json())
-			.then(response => response as TBody);
+		const init: RequestInit = {
+			method: methodType,
+			credentials: 'include',
+			headers: {
+				'Content-type': 'application/json'
+			}
+		}
+
+		if (body) {
+			init.body = JSON.stringify(body)
+		}
+
+		return fetch(this.basePath + '/' + path, init)
+			.then(response => {
+				return new Promise(async (resolve, reject) => {
+
+					if (response.ok) {
+						const hasContent = response.headers.has('Content-type')
+
+						if (!hasContent) {
+							resolve(response.status as TResponse)
+							return
+						}
+
+						const result = await response.json()
+						resolve(result)
+						return
+					}
+
+					const text = await response.text()
+					const error = {
+						response: {
+							data: JSON.parse(text)
+						}
+					}
+					reject(error)
+				})
+			})
+			.catch(e => {
+				//ALERT
+				throw e;
+			}) as Promise<TResponse>
 	}
-	
 
+	protected _get<TResponse>(path: string): Promise<TResponse> {
+		return this.baseRequest(path, 'GET')
+	}
 
+	protected _post<TResponse, TBody>(path: string, body: TBody): Promise<TResponse> {
+		return this.baseRequest(path, 'POST', body)
+	}
+
+	protected _put<TResponse, TBody>(path: string, body: TBody): Promise<TResponse> {
+		return this.baseRequest(path, 'PUT', body)
+	}
+
+	protected _delete<TResponse>(path: string) {
+		return this.baseRequest(path, 'DELETE');
+	}
 }
