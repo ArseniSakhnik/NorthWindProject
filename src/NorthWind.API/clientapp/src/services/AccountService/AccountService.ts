47484e4d9912e634de072f28d199a85a86958e60
sakhn <sakhnikarseni@mail.ru>
2022-01-20 06:46:53 +0300

    # Сервис регистрации

---
+++ src/NorthWind.API/clientapp/src/services/AccountService/AccountService.ts
@@ -0,0 +1,25 @@
+﻿import HttpService from "@/services/HttpService"
+import {ConfirmEmailModel, LoginModel, RegisterModel} from "@/services/AccountService/RequestsAccountService"
+
+export default class AccountService extends HttpService {
+
+	constructor() {
+		super('account')
+	}
+
+	Login(loginModel: LoginModel): Promise<void> {
+		return this._post('login', loginModel)
+	}
+
+	Register(registerModel: RegisterModel): Promise<void> {
+		return this._post('register', registerModel)
+	}
+
+	Logout(): Promise<void> {
+		return this._post('logout', {})
+	}
+
+	ConfirmEmail(confirmEmailModel: ConfirmEmailModel): Promise<void> {
+		return this._post('confirm-email', confirmEmailModel)
+	}
+}
---
+++ src/NorthWind.API/clientapp/src/services/AccountService/RequestsAccountService.ts
@@ -0,0 +1,16 @@
+﻿export type LoginModel = {
+	email: string,
+	password: string,
+	rememberMe: boolean
+}
+
+export type RegisterModel = {
+	email: string,
+	password: string,
+	confirmPassword: string
+}
+
+export type ConfirmEmailModel = {
+	userId: string,
+	code: string
+}
