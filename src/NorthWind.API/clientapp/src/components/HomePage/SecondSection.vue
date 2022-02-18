171d9f53fa1ef683262764f726a7bbc64d1c9144
sakhn <sakhnikarseni@mail.ru>
2022-02-18 20:43:43 +0300

    # Добавил блоки о компании

--- src/NorthWind.API/clientapp/src/components/HomePage/SecondSection.vue
+++ src/NorthWind.API/clientapp/src/components/HomePage/SecondSection.vue
@@ -1,23 +1,170 @@
 <template>
   <div class="second-section">
-    <about-service/>
+    <v-container>
+      <v-btn text color="#FF9B52">
+        <v-icon left>
+          mdi-arrow-up-left
+        </v-icon>
+        На главную
+      </v-btn>
+      <v-row>
+        <v-col
+            cols="12"
+            lg="6"
+        >
+          <h2 class="text-white about-font">ВЫВОЗ СТРОИТЕЛЬНОГО И КРУПНОГАБАРИТНОГО МУСОРА</h2>
+        </v-col>
+        <v-col
+            cols="12"
+            lg="6"
+        >
+          <h3 class="about-company"><span class="company-font">ООО "Северный Ветер"</span> предоставляет услуги
+            специализированной техники по
+            вывозу и
+            ликвидировании отходов твердого и жидкого типа в Крыму. </h3>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+            style="width: 100%"
+            cols="12"
+            lg="4"
+        >
+          <div class="about-company-block">
+            <Time class="about-company-icon"/>
+            <h4>
+              Удобное время
+            </h4>
+            <p>
+              Учитывая сложность транспортной инфраструктуры,
+              сотрудники нашей компании оказывают услуги в удобное для
+              Заказчика время как в дневное, так и в ночное.
+            </p>
+          </div>
+        </v-col>
+        <v-col
+            cols="12"
+            lg="4"
+        >
+          <div class="about-company-block">
+            <Reward class="about-company-icon"/>
+            <h4>Качество исполнения</h4>
+            <p>
+              Основным принципом, которым руководствуется все,
+              без исключения, сотрудники нашей компании, является
+              качество исполнения поставленных задач.
+            </p>
+          </div>
+        </v-col>
+        <v-col
+            cols="12"
+            lg="4"
+        >
+          <div class="about-company-block">
+            <Customer class="about-company-icon" style="margin-right: 0"/>
+            <h4>
+              Индивидуальный подход
+            </h4>
+            <p>
+              При долгосрочном сотрудничестве,
+              ООО «Северный Ветер», гарантирует индивидуальный подход к Заказчику.
+            </p>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script lang="ts">
 import {Vue, Component} from 'vue-property-decorator'
 import AboutService from "@/components/HomePage/secondSection/AboutService.vue";
+import Customer from "@/icons/Customer.vue";
+import Land from "@/icons/Land.vue";
+import RecyclingCar from "@/icons/RecyclingCar.vue";
+import Reward from "@/icons/Reward.vue";
+import Time from "@/icons/Time.vue";
+import RecyclingHome from "@/icons/RecyclingHome.vue";
+
 @Component({
-  components: {AboutService}
+  components: {RecyclingHome, AboutService, Customer, Land, RecyclingCar, Reward, Time}
 })
 export default class SecondSection extends Vue {
 }
 </script>
 
-<style scoped>
-.second-section{
+<style scoped lang="scss">
+.second-section {
   height: 120vh;
   background: linear-gradient(180deg, #003366 0%, #002347 100%),
   radial-gradient(21.56% 41.41% at 17.53% 51.62%, #003F7D 0%, rgba(0, 63, 125, 0) 100%);
+}
+
+.about-font {
+  font-family: 'Raleway', 'serif';
+  font-style: normal;
+  font-weight: 800;
+  font-size: 1em;
+}
+
+.about-company {
+  font-family: 'Montserrat', 'serif';
+  font-style: normal;
+  font-weight: normal;
+  font-size: 0.5em;
+  /* or 27px */
+
+  letter-spacing: 0.1em;
+  color: #F2F6FF;
+}
+
+.company-font {
+  color: #FF9B52;
+}
+
+.about-company-block {
+  
+  
+  background: linear-gradient(179.94deg, rgba(93, 124, 204, 0.2) 3.9%, rgba(255, 255, 255, 0) 123.19%);
+  filter: drop-shadow(-14px 14px 41px rgba(0, 17, 34, 0.5));
+  backdrop-filter: blur(9px);
+  border-radius: 7px;
+
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  align-content: center;
+  
+  
+  
+  padding: 0.5em;
+  
+  h4 {
+    font-size: 0.8em;
+    color: white;
+    font-family: 'Raleway', 'serif';
+    font-style: normal;
+    font-weight: bold;
+    text-align: center;
+  }
+  
+  p {
+    padding-top: 0.3em;
+    font-size: 0.5em;
+    font-family: 'Montserrat', 'serif';
+    font-style: normal;
+    font-weight: normal;
+    text-align: center;
+    color: #FFFFFF;
+  }
+  
+  height: 100%;
+
+}
+
+.about-company-icon {
+  height: 10vh;
 }
 </style>
