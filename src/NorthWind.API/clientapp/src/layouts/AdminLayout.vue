b8ca486d5e5769640e7cc30625c8b750d6682bde
sakhn <sakhnikarseni@mail.ru>
2022-04-06 20:04:16 +0300

    # удалил ненужные компоненты

--- src/NorthWind.API/clientapp/src/layouts/AdminLayout.vue
+++ src/NorthWind.API/clientapp/src/layouts/AdminLayout.vue
@@ -1,23 +1,15 @@
 ﻿<template>
   <v-app>
-    <admin-navbar/>
-    <admin-sidebar/>
-    <admin-actionbar/>
     <v-main>
       <router-view/>
     </v-main>
-    <admin-footer/>
   </v-app>
 </template>
 
 <script lang="ts">
 import {Vue, Component} from 'vue-property-decorator'
-import AdminNavbar from '@/components/Admin/AdminNavbar.vue'
-import AdminSidebar from '@/components/Admin/AdminSidebar.vue'
-import AdminActionbar from '@/components/Admin/AdminActionbar.vue'
-import AdminFooter from '@/components/Admin/AdminFooter.vue'
 
-@Component({components: {AdminNavbar, AdminSidebar, AdminActionbar, AdminFooter}})
+@Component({components: {}})
 export default class AdminLayout extends Vue {
 
 }
